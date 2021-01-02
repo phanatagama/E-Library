dfded3d4a4ab6b923f42572e52d63f977f96c0e7
I'zaz Dhiya 'Ulhaq <59567289+izazdhiya@users.noreply.github.com>
2021-01-02 07:22:05 +0700

    Update database sql

--- myflaskapp/Database/perpustakaan.sql
+++ myflaskapp/Database/perpustakaan.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 18, 2020 at 08:04 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.10
+-- Generation Time: Jan 02, 2021 at 01:18 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -32,19 +33,23 @@ CREATE TABLE `books` (
   `judul` varchar(100) NOT NULL,
   `pengarang` varchar(50) NOT NULL,
   `penerbit` varchar(50) NOT NULL,
-  `tahun` year(4) NOT NULL
+  `tahun` year(4) NOT NULL,
+  `rak` int(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`id`, `judul`, `pengarang`, `penerbit`, `tahun`) VALUES
-(2, 'Dilan 1998', 'Joko Widodo', 'PT. Gramedia', 2018),
-(4, 'Komik One Piece', 'Eichiiro Oda', 'Shueisha', 1998),
-(5, 'Harry Potter', 'J.K. Rowling', 'Warner Bross', 2005),
-(7, 'Cerita Rakyat', 'David', 'PT. Gadgetin', 2016),
-(8, 'Secangkir Kopi', 'Uut Dwisetya', 'Jawa Pos', 2017);
+INSERT INTO `books` (`id`, `judul`, `pengarang`, `penerbit`, `tahun`, `rak`) VALUES
+(2, 'Dilan 1998', 'Joko Widodo', 'PT. Gramedia', 2018, 1),
+(4, 'Komik One Piece', 'Eichiiro Oda', 'Shueisha', 1998, 5),
+(5, 'Harry Potter', 'J.K. Rowling', 'Warner Bross', 2005, 1),
+(7, 'Cerita Rakyat', 'David', 'PT. Gadgetin', 2016, 2),
+(8, 'Secangkir Kopi', 'Uut Dwisetya', 'Jawa Pos', 2017, 1),
+(9, '7 Keajaiban Rezeki', 'Ippho Santosa', 'Gramedia', 2010, 3),
+(10, 'hewan', 'robert', 'jasa', 2018, 2),
+(13, 'Belajar Pemrograman', 'Aga', 'univ', 2020, 4);
 
 -- --------------------------------------------------------
 
@@ -61,24 +66,49 @@ CREATE TABLE `borrow` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `borrow`
+-- Triggers `borrow`
 --
-
-INSERT INTO `borrow` (`id`, `id_buku`, `id_students`, `id_staff`, `tanggal_pinjam`) VALUES
-(9, 7, 2, 1, '2020-12-18');
+DELIMITER $$
+CREATE TRIGGER `inserttoriwayat` AFTER INSERT ON `borrow` FOR EACH ROW INSERT INTO riwayatpeminjaman SET id_buku=NEW.id_buku, id_student=NEW.id_students, id_staff=NEW.id_staff, tanggal_pinjam=NEW.tanggal_pinjam
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `returnbook`
+-- Table structure for table `riwayatpeminjaman`
 --
 
-CREATE TABLE `returnbook` (
+CREATE TABLE `riwayatpeminjaman` (
   `id` int(11) NOT NULL,
-  `id_borrow` int(11) NOT NULL,
-  `tanggal_kembali` date NOT NULL,
-  `denda` int(11) NOT NULL
+  `id_buku` int(11) NOT NULL,
+  `id_student` int(11) NOT NULL,
+  `id_staff` int(11) NOT NULL,
+  `tanggal_pinjam` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `riwayatpeminjaman`
+--
+
+INSERT INTO `riwayatpeminjaman` (`id`, `id_buku`, `id_student`, `id_staff`, `tanggal_pinjam`) VALUES
+(1, 2, 2, 1, '2020-12-26'),
+(2, 4, 1, 1, '2020-12-27'),
+(3, 2, 2, 1, '2020-12-27'),
+(4, 5, 1, 1, '2020-12-27'),
+(5, 9, 1, 1, '2020-12-27'),
+(6, 10, 2, 1, '2020-12-27'),
+(7, 4, 2, 1, '2020-12-27'),
+(8, 10, 1, 1, '2020-12-27'),
+(9, 9, 1, 1, '2020-12-27'),
+(10, 9, 1, 1, '2020-12-27'),
+(11, 4, 2, 1, '2020-12-27'),
+(12, 10, 2, 1, '2020-12-27'),
+(13, 4, 1, 1, '2020-12-27'),
+(14, 9, 2, 1, '2020-12-27'),
+(15, 4, 1, 1, '2020-12-27'),
+(16, 10, 1, 1, '2020-12-27'),
+(17, 13, 1, 1, '2020-12-30');
 
 -- --------------------------------------------------------
 
@@ -92,15 +122,16 @@ CREATE TABLE `staff` (
   `nip` varchar(20) NOT NULL,
   `email` varchar(50) NOT NULL,
   `contact` varchar(20) NOT NULL,
-  `alamat` varchar(100) NOT NULL
+  `alamat` varchar(100) NOT NULL,
+  `pass` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `staff`
 --
 
-INSERT INTO `staff` (`id`, `nama`, `nip`, `email`, `contact`, `alamat`) VALUES
-(1, 'Hendra', '232456', 'hendra@gmail.com', '081523499778', 'Jl. Jawa 7 - Jember');
+INSERT INTO `staff` (`id`, `nama`, `nip`, `email`, `contact`, `alamat`, `pass`) VALUES
+(1, 'Hendra', '232456', 'hendra@gmail.com', '081523499778', 'Jl. Jawa 7 - Jember', 'abc12345');
 
 -- --------------------------------------------------------
 
@@ -145,11 +176,10 @@ ALTER TABLE `borrow`
   ADD KEY `id_staff` (`id_staff`);
 
 --
--- Indexes for table `returnbook`
+-- Indexes for table `riwayatpeminjaman`
 --
-ALTER TABLE `returnbook`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_borrow` (`id_borrow`);
+ALTER TABLE `riwayatpeminjaman`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `staff`
@@ -171,19 +201,19 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `borrow`
 --
 ALTER TABLE `borrow`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
--- AUTO_INCREMENT for table `returnbook`
+-- AUTO_INCREMENT for table `riwayatpeminjaman`
 --
-ALTER TABLE `returnbook`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `riwayatpeminjaman`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `staff`
@@ -208,12 +238,6 @@ ALTER TABLE `borrow`
   ADD CONSTRAINT `borrow_ibfk_1` FOREIGN KEY (`id_buku`) REFERENCES `books` (`id`),
   ADD CONSTRAINT `borrow_ibfk_2` FOREIGN KEY (`id_students`) REFERENCES `students` (`id`),
   ADD CONSTRAINT `borrow_ibfk_3` FOREIGN KEY (`id_staff`) REFERENCES `staff` (`id`);
-
---
--- Constraints for table `returnbook`
---
-ALTER TABLE `returnbook`
-  ADD CONSTRAINT `returnbook_ibfk_1` FOREIGN KEY (`id_borrow`) REFERENCES `borrow` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
